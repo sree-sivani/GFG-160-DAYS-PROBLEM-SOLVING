42d176fc611936693ca220baa2bed9bb5585b36e
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-01-24 19:20:52 +0530

    Create day68.java
    Linked List Group Reverse

---
+++ day68.java
@@ -0,0 +1,51 @@
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    public static Node reverseKGroup(Node head, int k) {
+        // Your code here
+        if(head==null || head.next==null) return head;
+        Node t=head,n=null,p=null;
+        while(t!=null){
+            int l=1;
+            while(t.next!=null && l<k){
+                t=t.next;
+                l++;
+            }
+            if(p!=null) p.next=t;
+            if(n==null){
+                p=head;
+                head=t;
+            }
+            else p=n;
+            n=t.next;
+            t.next=null;
+            reverse(p);
+            p.next=n;
+            t=n;
+        }
+        return head;
+    }
+    static Node reverse(Node head){
+        if(head==null || head.next==null) return head;
+        Node t=head,p=null;
+        while(t!=null){
+            Node n =t.next;
+            t.next=p;
+            p=t;
+            t=n;
+        }
+        return p;
+    }
+}
