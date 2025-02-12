13c6fe21ffe557cc20b0386086e3721c6465a843
sree-sivani <srisivanimvs@gmail.com>
2025-02-12 09:04:50 +0530

    k-th Smallest in BST

---
+++ day90.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    // Return the Kth smallest element in the given BST
+    ArrayList<Integer>arr=new ArrayList<>();
+    public void BST(Node root)
+    {
+        if(root!=null){
+        arr.add(root.data);
+        BST(root.left);
+        BST(root.right);
+        }
+        else
+        return;
+    }
+    public int kthSmallest(Node root, int k) {
+        // Write your code here
+        BST(root);
+        Collections.sort(arr);
+        if(k>arr.size())
+        return -1;
+        return arr.get(k-1);
+    }
+}
