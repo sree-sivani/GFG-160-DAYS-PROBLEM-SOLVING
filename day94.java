0b1f6a1dcf83ab972e5c507e6b557bdafc865c1f
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-02-18 09:31:58 +0530

    Create day94.java
    Serialize and deserialize a binary tree

---
+++ day94.java
@@ -0,0 +1,23 @@
+/*class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}*/
+
+class Tree {
+    // Function to serialize a tree and return a list containing nodes of tree.
+    public ArrayList<Integer> serialize(Node root) {
+        // code here
+    }
+
+    // Function to deserialize a list and construct the tree.
+    public Node deSerialize(ArrayList<Integer> arr) {
+        // code here
+    }
+};
